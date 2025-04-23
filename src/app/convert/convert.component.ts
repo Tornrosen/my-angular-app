06b665ebc8a56c376e02b0a0adb7487a4dfc53c2
Tornrosen <elec11anna@hotmail.com>
2025-04-23 17:45:30 +0200

    skapa startsida och omvandlingssida

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,11 +1,37 @@
+import { DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule, DecimalPipe],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.scss'
 })
 export class ConvertComponent {
+meters:number=0;
+feet:number=0;
+feetAnswer:number=0;
+metersAnswer:number=0;
 
+celcius:number=0;
+farenheit:number=0;
+farenheitAnswer:number=0;
+celciusAnswer:number=0;
+
+convertToFeet():void {
+  this.feetAnswer=this.meters*3.28;
+}
+
+convertToMeters():void {
+  this.metersAnswer=this.feet/3.28;
+}
+
+convertToFarenheit():void {
+  this.farenheitAnswer=this.celcius* (9/5) + 32
+}
+
+convertToCelcius(): void {
+  this.celciusAnswer=(this.farenheit-32)*5/9
+}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+moreSpringImg: string = "images/moreSpring.jpg";
 }
